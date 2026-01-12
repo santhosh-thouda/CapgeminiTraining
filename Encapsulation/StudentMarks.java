4057ef080edf5b71d3ad34fa25e3df75b31eb3b9
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2026-01-13 01:21:32 +0530

    Delete Encapsulation directory

--- Encapsulation/StudentMarks.java
+++
@@ -1,9 +0,0 @@
-package Encapsulation;
-
-public class StudentMarks {
-	public static void main(String[] args) {
-		StudentMarks1 s = new StudentMarks1();
-		s.setMarks(91);
-		System.out.println(s.getMarks());
-	}
-}
